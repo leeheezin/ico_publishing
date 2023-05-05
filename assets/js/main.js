47fa45f2c6abdf052dc1f4a51f2cbade6c0becbc
leeheezin <gmlwls107@naver.com>
2023-05-05 23:19:17 +0900

    sidebar만듬

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,25 @@
-var container = document.querySelector('.locaMap');
-var options = {
-  center: new kakao.maps.LatLng(37.566826, 126.9786567),
-  level: 3
-};
+//mobilemenu
+$(document).ready(function(){
+ 
+  $('.menu_btn>a').on('click', function(){
+      $('.menu_bg').show(); 
+      $('.sidebar_menu').show().animate({
+          right:0
+      });  
+  });
+  $('.close_btn>a').on('click', function(){
+      $('.menu_bg').hide(); 
+      $('.sidebar_menu').animate({
+          right: '-' + 50 + '%'
+                  },function(){
+                      $('.sidebar_menu').hide();          
+                  });  
+  });
 
-var map = new kakao.maps.Map(container, options);
+});
+
+
+
+
+
+
