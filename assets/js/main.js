0f8318b24fb2d8218b7a592d6106f4bbab7e696d
leeheezin <gmlwls107@naver.com>
2023-05-09 18:00:43 +0900

    사이드바고치기

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,26 @@
 //mobilemenu
 $(document).ready(function(){
- 
-  $('.menu_btn>a').on('click', function(){
-      $('.menu_bg').show(); 
-      $('.sidebar_menu').show().animate({
-          right:0
-      });  
+    var scrollPos = 0;
+  
+    $('.menu_btn>a').on('click', function(){
+        scrollPos = $(window).scrollTop();
+        $('.menu_bg').show(); 
+        $('.sidebar_menu').show().animate({
+            right:0
+        });  
+    });
+  
+    $('.close_btn>a').on('click', function(){
+        $('.menu_bg').hide(); 
+        $('.sidebar_menu').animate({
+            right: '-' + 50 + '%'
+        }, function(){
+            $('.sidebar_menu').hide();  
+            $(window).scrollTop(scrollPos); // 스크롤 위치 이동
+        });
+    });
   });
-  $('.close_btn>a').on('click', function(){
-      $('.menu_bg').hide(); 
-      $('.sidebar_menu').animate({
-          right: '-' + 50 + '%'
-                  },function(){
-                      $('.sidebar_menu').hide();          
-                  });  
-  });
-
-});
-
-
-
+  
 
 
 
